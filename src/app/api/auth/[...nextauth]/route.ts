b27ee9e13e57dedd4898299cd0a3f238fa529668
ettige <latifishal@gmail.com>
2023-08-04 21:47:51 +0330

    add Credential

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -19,10 +19,46 @@ const authOptions: AuthOptions = {
     CredentialsProvider({
       name: 'Credentials',
       credentials: {
-        email: { label: "Email", type: "email"},
+        email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" }
       },
       async authorize(credentials, req) {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        //check if email pattern
+        if (credentials && emailRegex.test(String(credentials?.email))) {
+          const credentialdb = await prisma.credential.findUnique({
+            where: {
+              email: credentials?.email
+            }
+          })
+          if (credentialdb) {
+
+            if (credentialdb?.password === credentials?.password) {
+              const user = await prisma.user.findUnique({
+                where: {
+                  id: credentialdb.userId
+                }
+              })
+              return user;
+            }
+          }
+          else {
+
+            const newUser = await prisma.user.create({
+              data: {
+                email: credentials.email
+              }
+            })
+            const newcredential = await prisma.credential.create({
+              data: {
+                userId: newUser.id,
+                email: credentials?.email,
+                password: credentials?.password
+              }
+            })
+            return newUser
+          }
+        }
         return null
       }
     })
@@ -34,16 +70,19 @@ const authOptions: AuthOptions = {
     async jwt({ session, token, user }) {
       if (user) {
         token.id = user.id;
-        token.name=user.name;
+        token.name = user.name;
         token.email = user.email;
       }
       return token;
     },
     async session({ session, token, user }) {
       // Send properties to the client, like an access_token and user id from a provider.
-      
+
       return session
     }
+  },
+  pages: {
+    signIn: "/"
   },
   adapter: PrismaAdapter(prisma),
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 'use client'
-import { useSession, signIn, signOut } from 'next-auth/react'
-import Image from 'next/image'
-export default function Home() {
-  const { data: session, status, update } = useSession()
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
+import { useState, ChangeEvent, FormEvent } from 'react';
 
+interface FormState {
+  email: string;
+  password: string;
+}
+
+export default function Home() {
+  const [form, setForm] = useState<FormState>({
+    email: "",
+    password: ""
+  });
+
+  const { data: session, status, update } = useSession();
+
+  const formChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
+  }
+
+  const formSubmit = (event: FormEvent) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    signIn("credentials", { email: form.email, password: form.password });
+  }
   if (status == "authenticated") {
     return (
       <main className="container text-center justify-center flex">
@@ -36,6 +57,19 @@ export default function Home() {
           <div className='flex flex-col gap-2'>
             <button className='btn btn-wide btn-neutral' onClick={() => signIn('github')}>Github</button>
             <button className='btn btn-wide bg-indigo-700 hover:bg-indigo-600' onClick={() => signIn('discord')}>Discord</button>
+          </div>
+          <div onChange={formChange}>
+            <div className="input-group-sm flex flex-col">
+              <label htmlFor="email">Email</label>
+              <input placeholder='me@ex.ample' type="email" name='email' className="input-sm input-accent" />
+            </div>
+            <div className="input-group-sm flex flex-col">
+              <label htmlFor="password">password</label>
+              <input type="password" name='password' className="input-sm input-accent" />
+            </div>
+            <div>
+              <button className="btn btn-wide" onClick={formSubmit}>Sign In</button>
+            </div>
           </div>
         </div>
       </main>
